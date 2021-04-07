cca48f19671511e41e58640610e4174d46102b08
Ornella Stirparo <ornellaluz@hotmail.com>
2021-04-07 20:49:23 -0300

    Ejercicio de practica 'beers'

--- Ejercicios/beers.js
+++ Ejercicios/beers.js
@@ -19,3 +19,51 @@ const beers = [
     { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
     { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
   ];
+  
+   //primero
+   const PROMO = 'Purple Iris';
+   const PRECIO_BAJO = 300;
+   const PRECIO_ALTO = 350;
+   const PRECIO_PROMO = 320;
+ 
+   function precioPorCriterio() {
+     console.log("1. PRECIO POR GRADUACION ALCOHOLICA");
+     let lista = beers.filter(beer => beer.abv < 5 && beer.name != PROMO).map(beer => ({
+         ...beer,
+         Precio: PRECIO_BAJO
+       }))
+       let lista1 = beers.filter(beer => beer.abv >= 5 && beer.name != PROMO).map(beer => ({
+         ...beer,
+         Precio: PRECIO_ALTO
+       }))
+     let lista2 = beers.filter(beer => beer.name == PROMO).map(beer => ({
+       ...beer,
+       Precio: PRECIO_PROMO
+     }))
+       let listaFinal = lista.concat(...lista1, ...lista2);
+     return listaFinal;
+   }
+   console.log(precioPorCriterio());
+ 
+   //segundo
+ function insertarFileName() {
+   console.log("2. INSERTAR FILE_NAME");
+ let insert = beers.map(beer => ({...beer, 
+   file_name: beer.label.split('/')[6]}))  
+ return insert;
+ }
+ console.log(insertarFileName());
+ 
+ 
+   //tercero
+   function compare(a, b) {
+     if ( a.type < b.type ){
+       return -1;
+     }
+     if ( a.type > b.type ){
+       return 1;
+     }
+     return 0;
+   }
+   console.log("3. ORDENADAS POR TIPO");
+   console.log(beers.sort(compare));
